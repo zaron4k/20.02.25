b1ae01dd0475136ffb4fcc518310357a26bf6c36
zaron4k <76218026+zaron4k@users.noreply.github.com>
2025-02-20 11:00:20 +0200

    Add files via upload

---
+++ ConsoleApp1/Avto.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Avto
+    {
+        private string brand;
+        private string color;
+        private double skor;
+
+        public string Brand
+        { get { return brand;} 
+          set 
+            {
+               brand = value; 
+            } 
+        }
+        public string Color
+        { get { return color; } 
+          set 
+            { 
+               color = value; 
+            } 
+        }
+        public double Skor 
+        { 
+            get { return skor;} 
+            set 
+            { 
+                if (value < 20) 
+                    skor = 20; 
+                else if (value > 120) 
+                    skor = 120;
+                else skor = value;
+            } 
+        }
+
+        /// <summary>
+        /// Конструктор без параметров
+        /// </summary>
+        public Avto ()
+        { }
+        /// <summary>
+        /// Конструктор с параметрами
+        /// </summary>
+        /// <param name="brond"></param>
+        public Avto (string brand, string color, double skor)
+        {
+            this.Brand = brand;
+            this.Color = color;
+            this.Skor = skor;
+        }
+        public override string ToString()
+        {
+            return $"Марка: {Brand}\nЦвет: {Color}\nСкорость: {Skor} км/ч";
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Avto car1 = new Avto();
+            car1.Brand = "Лада";
+            car1.Color = "розовый";
+            car1.Skor = 70.2;
+            Console.WriteLine($"{car1.ToString()}");
+            Console.WriteLine();
+            Avto car2 = new Avto("Ламборгини","красный",100);
+            Console.WriteLine($"{car2.ToString()}");
+            Console.WriteLine();
+
+            Console.Write("Название марки: ");
+            string brand = Console.ReadLine();
+            Console.Write("Цвет: ");
+            string color = Console.ReadLine();
+            Console.Write("Скорость: ");
+            double skor = double.Parse(Console.ReadLine());
+            Avto car3 = new Avto(brand,color,skor);
+            Console.WriteLine($"{car3.ToString()}");
+            Console.ReadLine();
+        }
+    }
+}
